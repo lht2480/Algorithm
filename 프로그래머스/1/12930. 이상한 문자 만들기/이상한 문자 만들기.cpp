c314098eafd4519fbc15d55eef11c02b85e03eab
lht2480 <jgjjgiggk@gmail.com>
2025-06-30 09:39:50 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.cpp
@@ -0,0 +1,41 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) 
+{
+    string answer = "";
+    int i = 0;
+    for(char c : s)
+    {
+        if(c == ' ')
+        {
+            answer += c;
+            i = 0;
+        }else
+        {
+            if(i % 2 == 0)
+            {
+                if(c >= 'a'&& c <= 'z')
+                {
+                    answer += c - 32;
+                }else
+                {
+                    answer += c;
+                }
+            }else
+            {
+                if(c >= 'A' && c <= 'Z')
+                {
+                    answer += c + 32;
+                }else
+                {
+                    answer += c;
+                }
+            }
+            i++;
+        }
+    }
+    return answer;
+}
