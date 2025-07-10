f2ab89b2e3c5b229d1513a2d030d13f3163002af
lht2480 <jgjjgiggk@gmail.com>
2025-07-10 11:44:12 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.85 ms, Memory: 3.81 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.cpp
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
 #include <vector>
-#include <queue>
+#include <algorithm>
 using namespace std;
 
 vector<int> solution(int k, vector<int> score) {
     vector<int> answer;
-    priority_queue<int, vector<int>, greater<int>> pq; // 최소 힙
+    vector<int> honor;
 
-    for (int i = 0; i < score.size(); ++i) {
-        if (pq.size() < k) {
-            pq.push(score[i]); // 명예의 전당 자리가 남았으면 그냥 추가
-        } else if (score[i] > pq.top()) {
-            pq.pop();          // 가장 낮은 점수 제거
-            pq.push(score[i]); // 새로운 점수 추가
+    for (int s : score) {
+        honor.push_back(s);
+        sort(honor.begin(), honor.end(), greater<int>());
+
+        if (honor.size() > k) {
+            honor.pop_back();
         }
-        answer.push_back(pq.top()); // 명예의 전당 중 최하 점수 출력
+
+        answer.push_back(honor.back());
     }
 
     return answer;
