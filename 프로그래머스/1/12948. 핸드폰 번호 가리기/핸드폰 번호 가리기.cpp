c655bd620165e94d839670ec5bfb171be6899b27
lht2480 <jgjjgiggk@gmail.com>
2025-07-17 09:23:17 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+//01023952480
+string solution(string phone_number) {
+    string answer = "";
+    if(phone_number.length()==4)
+    {
+        for(int i = phone_number.length()-4; i < phone_number.length(); i++)
+        {
+            answer += phone_number[i];
+        }
+    }
+    for(int i = 0; i < phone_number.length()-4; i++)
+    {
+        answer += "*";
+    }
+    if(phone_number.length() > 4)
+    {
+        for(int i = phone_number.length()-4; i < phone_number.length(); i++)
+        {
+            answer += phone_number[i];
+        }
+    }
+    return answer;
+}
