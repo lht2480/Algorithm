555e564b3bbeb36f3281c46343302cc612d99ec9
lht2480 <jgjjgiggk@gmail.com>
2025-07-17 09:10:01 +0900

    [level 1] Title: 약수의 합, Time: 0.02 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) 
+{
+    int answer = 0;
+    for(int a=1;a<=n;a++)
+    {
+        if((n%a)==0)
+        {
+            answer = answer + a;
+        }
+    }
+    return answer;
+    return answer;
+}
