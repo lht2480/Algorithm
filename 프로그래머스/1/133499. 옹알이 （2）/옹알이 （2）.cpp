372bbc9cac4cb5e307efb7f76d656eea13d0aa3f
lht2480 <jgjjgiggk@gmail.com>
2025-07-22 09:39:07 +0900

    [level 1] Title: 옹알이 (2), Time: 0.06 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.cpp
@@ -0,0 +1,40 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool isValid(string word)
+{
+    vector<string> sound = {"aya", "ye", "woo", "ma"};
+    string temp = "";
+    
+    while(!word.empty())
+    {
+        bool matched = false;
+        for(string s : sound)
+        {
+            if(word.find(s) == 0)
+            {
+                if(s == temp) return false;
+                temp = s;
+                word = word.substr(s.length());
+                matched = true;
+                break;
+            }
+        }
+        if(!matched) return false;
+    }
+    return true;
+}
+
+int solution(vector<string> babbling) 
+{
+    int answer = 0;
+    
+    for (string word : babbling) 
+    {
+        if (isValid(word)) answer++;
+    }
+    return answer;
+}
