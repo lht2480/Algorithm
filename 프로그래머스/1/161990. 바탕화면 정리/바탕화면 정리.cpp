ff82b697d6334034e69cf8feffc3b2f9acab14ea
lht2480 <jgjjgiggk@gmail.com>
2025-07-29 10:01:55 +0900

    Update 바탕화면 정리.cpp

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.cpp
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.cpp
@@ -3,16 +3,20 @@
 
 using namespace std;
 
-vector<int> solution(vector<string> wallpaper) {
+vector<int> solution(vector<string> wallpaper) 
+{
     int n = wallpaper.size();
     int m = wallpaper[0].size();
 
     int lux = n, luy = m;
     int rdx = 0, rdy = 0;
 
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < m; ++j) {
-            if (wallpaper[i][j] == '#') {
+    for (int i = 0; i < n; ++i) 
+    {
+        for (int j = 0; j < m; ++j) 
+        {
+            if (wallpaper[i][j] == '#') 
+            {
                 lux = min(lux, i);
                 luy = min(luy, j);
                 rdx = max(rdx, i + 1);
