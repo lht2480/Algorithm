5579cd5f0fcc716e331d32a656a25c9c5f99495f
lht2480 <jgjjgiggk@gmail.com>
2025-08-13 09:42:39 +0900

    [level 1] Title: 모의고사, Time: 0.10 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.cpp
+++ 프로그래머스/1/42840. 모의고사/모의고사.cpp
@@ -4,42 +4,37 @@
 
 using namespace std;
 
-vector<int> solution(vector<int> answers) 
-{
+vector<int> solution(vector<int> answers) {
+    vector<int> answer;
+    vector<int> score(3, 0);
     vector<int> stu1 = {1,2,3,4,5};
     vector<int> stu2 = {2,1,2,3,2,4,2,5};
     vector<int> stu3 = {3,3,1,1,2,2,4,4,5,5};
-    vector<int> answer;
-    vector<int> score = {0,0,0};
-        
-    for(int i = 0; i < answers.size(); i++)
+    
+    for(int i = 0; i < answers.size(); ++i)
     {
-        if(stu1[i % stu1.size()] == answers[i])
+        if(answers[i] == stu1[i % stu1.size()])
         {
             score[0]++;
         }
-        if(stu2[i % stu2.size()] == answers[i])
+        if(answers[i] == stu2[i % stu2.size()])
         {
             score[1]++;
         }
-        if(stu3[i % stu3.size()] == answers[i])
+         if(answers[i] == stu3[i % stu3.size()])
         {
             score[2]++;
         }
     }
-    int maxscore = max({score[0], score[1], score[2]});
+  
+    int maxele = max({score[0], score[1], score[2]});
     
-    if(score[0] == maxscore)
+    for(int i = 0; i < score.size(); ++i)
     {
-         answer.push_back(1);
-    }
-    if(score[1] == maxscore)
-    {
-         answer.push_back(2);
-    }
-    if(score[2] == maxscore)
-    {
-         answer.push_back(3);
+        if(maxele == score[i])
+        {
+            answer.push_back(i+1);
+        }
     }
     return answer;
 }
